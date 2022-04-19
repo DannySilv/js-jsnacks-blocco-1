20b49d06f6291880736eb46b150d464237cbf7f9
DannySilv <danielesilvestre@outlook.it>
2022-04-19 18:16:33 +0200

    Completed Snack4

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -1,3 +1,15 @@
 // Chiedi un numero di 4 cifre all'utente
 // e calcola la somma di tutte le cifre che compongono il numero.
 
+let userNumber = prompt("Dammi un numero di 4 cifre");
+console.log(userNumber);
+let sum = 0;
+
+while (userNumber) {
+    sum += userNumber % 10;
+    console.log(sum);
+    userNumber = Math.floor(userNumber / 10);
+    console.log(userNumber);
+}
+
+console.log(sum)
